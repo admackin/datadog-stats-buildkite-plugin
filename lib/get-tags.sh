a345c49789fb311f131c6e3a6e5086fcb653819d
Andy MacKinlay <admackin@users.noreply.github.com>
2020-11-13 14:54:35 +1100

    clean up bk label

--- lib/get-tags.sh
+++ lib/get-tags.sh
@@ -27,7 +27,7 @@ function readAdditionalTags() {
 }
 
 function getTags() {
-  BK_LABEL=${BUILDKITE_LABEL}
+  BK_LABEL="$(echo "$BUILDKITE_LABEL" | tr -d '[[:punct:]]' | tr ' ' '_' | tr '[A-Z]' '[a-z]')"
   BK_AGENT_QUEUE=${BUILDKITE_AGENT_META_DATA_QUEUE}
 
   IS_MASTER=false
